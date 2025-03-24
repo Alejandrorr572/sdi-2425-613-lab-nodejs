4193ed2c200d65ec15666ad91b3522008b0fff0e
Alex <alexrivada@gmail.com>
2025-03-24 20:19:07 +0100

    SDI-2425-613-10.2-Redirección de peticiones.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -39,7 +39,6 @@ const userAudiosRouter = require('./routes/userAudiosRouter');
 app.use("/songs/add",userSessionRouter);
 app.use("/publications",userSessionRouter);
 app.use("/audios/",userAudiosRouter);
-app.use("/shop/",userSessionRouter);
 
 const userAuthorRouter = require('./routes/userAuthorRouter');
 app.use("/songs/edit",userAuthorRouter);
